533863bd1d8405d47f7e1e3e6f2d37dc75771dd5
Alex Sell <alextricityy@gmail.com>
2018-05-21 15:56:30 +0100

    final push

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'dogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :dogs
 end
