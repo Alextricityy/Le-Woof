0c89e1847375d1ac91bcd0382d6813a8e00ff1cf
Alex Sell <alextricityy@gmail.com>
2018-05-21 17:25:30 +0100

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'dogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
   resources :dogs
+end
 end
