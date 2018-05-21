accf1260a47fdf1887df79737befc29b772adec2
Alex Sell <alextricityy@gmail.com>
2018-05-21 17:00:48 +0100

    finished dog model

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,15 +1,21 @@
 class DogsController < ApplicationController
+  before_action :set_dog, except: :new
 
   def index
     @dogs = Dog.all
   end
 
   def new
-
+    @dog = Dog.new
   end
 
   def create
-
+    @dog = Dog.create(dog_params)
+    if @dog.save
+      redirect_to dogs_path
+    else
+      render "new"
+    end
   end
 
   def edit
@@ -17,19 +23,25 @@ class DogsController < ApplicationController
   end
 
   def update
-
+    @dog.update(dog_params)
+      if @dog.save
+      redirect_to dogs_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
-
+    @dog.delete
   end
 
   def show
+    @dog = Dog.find(params[:id])
 
   end
   private
   def set_dog
-    @dog = Dog.find(params[:id])
+     @dog = Dog.find(params[:id])
   end
 
   def dog_params
