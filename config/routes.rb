5a5c42ebbc989f239e77c689e4fc9e7dec0a7469
Alex Sell <alextricityy@gmail.com>
2018-05-21 17:47:31 +0100

    fixed links

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'dogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
   resources :dogs
-end
 end
