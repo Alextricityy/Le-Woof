0ba127b27cec96b2032f5a72c4f270c4012751f6
Hedi van Meurs <alisoun@gmail.com>
2018-05-23 14:11:09 +0100

    made a beautiful bookings show

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_22_155425) do
-
+ActiveRecord::Schema.define(version: 2018_05_22_162502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
