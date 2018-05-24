082891c2ce7493f249df3e01b0aa057ab36bae4e
Alex Sell <alextricityy@gmail.com>
2018-05-24 16:50:07 +0100

    commented js out

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    authorize @review
+    # raise
+    @review.user = current_user
+    @review.dog_id = params[:dog_id].to_i
+    if @review.save
+      redirect_to dog_path(params[:dog_id].to_i)
+    else
+      render "dogs/show"
+    end
+
+  end
+   private
+  def set_review
+     @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :dogs do
     resources :bookings, except: [:index, :new]
-    resources :reviews
+    resources :reviews, only: [:create]
   end
 end
